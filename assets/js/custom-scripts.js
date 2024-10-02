ff63882f695ec8093cbe2541494c5310bb6bb167
Israel Díaz <israel.diazrosas@outlook.com>
2024-10-02 12:46:39 -0300

    optimizaciones imagenes e importaciones

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-   $('a.page-scroll').bind('click', function(event) {
+$(document).ready(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-			scrollTop: ($($anchor.attr('href')).offset().top) 
+            scrollTop: ($($anchor.attr('href')).offset().top)
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -10,17 +10,17 @@ $(document).ready(function(){
 
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbarToggler'
-  })
+})
 
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggler:visible').click();
 });
 
-$(".nav-item .nav-link").on("click", function(){
+$(".nav-item .nav-link").on("click", function () {
     $(this).parent().addClass('active').siblings().removeClass('active');
- });
+});
 
- 
-$(window).on('activate.bs.scrollspy', function(event){
+
+$(window).on('activate.bs.scrollspy', function (event) {
     $(event.relatedTarget).parent().addClass('active').siblings().removeClass('active');
 });
