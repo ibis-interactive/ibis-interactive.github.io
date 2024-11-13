913bba45d47885decb9a839145734802f1cf6845
Israel Díaz <israel.diazrosas@outlook.com>
2024-11-13 18:08:29 -0300

    mobile friendlier, actualiza descripcion picoteo

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,45 +1,79 @@
 $(document).ready(function () {
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  //   // true for mobile device
+  //   console.log("mobile device");
+  //   $('.portfolio-card').on('click touchstart', (element) => {
+  //     // element.stopPropagation(); // Evita que el evento se propague a otros elementos
+  //     $(this).find('figcaption').toggleClass('active');
+  //   });
+  // }
 });
 
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbarToggler'
+  target: '#navbarToggler'
 })
 
 $('.navbar-collapse ul li a').click(function () {
-    $('.navbar-toggler:visible').click();
+  $('.navbar-toggler:visible').click();
 });
 
 $(".nav-item .nav-link").on("click", function () {
-    $(this).parent().addClass('active').siblings().removeClass('active');
+  $(this).parent().addClass('active').siblings().removeClass('active');
 });
 
 
 $(window).on('activate.bs.scrollspy', function (event) {
-    $(event.relatedTarget).parent().addClass('active').siblings().removeClass('active');
+  $(event.relatedTarget).parent().addClass('active').siblings().removeClass('active');
 });
 
 $.getJSON('./assets/games.json', function (data) {
-    console.log('reading json');
-    const games = data.games;
-    games.forEach(function (game) {
-        console.log('for loop');
+  const games = data.games;
+  games.forEach(function (game) {
 
-        const portfolioHtml = `
+    // const portfolioHtml = `
+    //     <div class="col-12 col-md-6 col-xl-4 mb-4">
+    //       <div class="grid overlay">
+    //         <figure class="portfolio-card" data-target="#${game.modalId}"> <img class="img-responsive" src="${game.imgSrc}" alt="${game.altText}">
+    //           <figcaption>
+    //             <h5>${game.title}</h5>
+    //             <span data-bs-toggle="modal" data-bs-target="#${game.modalId}" class="btn btn-default">Detalles</span>
+    //           </figcaption>
+    //         </figure>
+    //       </div>
+    //     </div>
+
+    //     <div class="modal fade" id="${game.modalId}" tabindex="-1" aria-hidden="true" aria-labelledby="${game.modalId}">
+    //       <div class="modal-dialog">
+    //         <div class="modal-content">
+    //           <div class="modal-header">
+    //             <h4 class="modal-title">${game.title}</h4>
+    //             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    //           </div>
+    //           <div class="modal-body centered">
+    //             <p><img class="img-responsive portfolio-card" src="${game.imgSrc}" alt="${game.altText}"></p>
+    //             <p class="modal-text">${game.description}</p>
+    //             <a href="${game.link}" class="btn btn-itchio">${game.playOn}</a>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   `;
+
+    const portfolioHtml = `
         <div class="col-12 col-md-6 col-xl-4 mb-4">
-          <div class="grid overlay">
-            <figure class="portfolio-card"> <img class="img-responsive" src="${game.imgSrc}" alt="${game.altText}">
-              <figcaption>
-                <h5>${game.title}</h5>
-                <span data-bs-toggle="modal" data-bs-target="#${game.modalId}" class="btn btn-default">Detalles</span>
-              </figcaption>
-            </figure>
+          <div class="grid">
+            <div class="portfolio-card">
+              <img class="img-responsive" src="${game.imgSrc}" alt="${game.altText}">
+              <div data-bs-toggle="modal" data-bs-target="#${game.modalId}" class="btn btn-default">${game.title}</div>
+            </div>
           </div>
         </div>
   
@@ -60,26 +94,26 @@ $.getJSON('./assets/games.json', function (data) {
         </div>
       `;
 
-        $("#gamesList").append(portfolioHtml);
-    });
+    $("#gamesList").append(portfolioHtml);
+  });
 });
 
 $.getJSON('./assets/team.json', function (data) {
-    let teamMembers = data.teamMembers;
-    teamMembers.forEach(member => {
-        let links = '';
-        member.links.forEach(link => {
-            links += `<a href="${link.href}" aria-label="${link.label}"><i class="fa ${link.icon}"></i></a>`;
-        });
+  let teamMembers = data.teamMembers;
+  teamMembers.forEach(member => {
+    let links = '';
+    member.links.forEach(link => {
+      links += `<a href="${link.href}" aria-label="${link.label}"><i class="fa ${link.icon}"></i></a>`;
+    });
 
-        let roles = '';
-        member.roles.forEach( (role, idx) => {
-            if (idx != 0) roles += " - "
-            roles += `${role.name} <span class="${role.icon}"/>`;
-        });
+    let roles = '';
+    member.roles.forEach((role, idx) => {
+      if (idx != 0) roles += " - "
+      roles += `${role.name} <span class="${role.icon}"/>`;
+    });
 
 
-        let memberHTML = `
+    let memberHTML = `
         <div class="col-lg-4 centered pad">
           <img class="img img-circle" src="${member.img}" height="120px" width="120px" alt="team member ${member.name}">
           <h4><strong>${member.name}</strong></h4>
@@ -88,6 +122,6 @@ $.getJSON('./assets/team.json', function (data) {
           ${links}
         </div>
       `;
-        $('#teamMembers').append(memberHTML);
-    });
+    $('#teamMembers').append(memberHTML);
+  });
 });
